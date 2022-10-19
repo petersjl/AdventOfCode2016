165dcabe8c6b0168cbf84f01c89478acfedad81e
Joseph Peters <petersjl@rose-hulman.edu>
2022-10-19 15:56:28 -0400

    Dart boilerplate

---
+++ DartUtils.dart
@@ -0,0 +1,14 @@
+import 'package:path/path.dart' as Path;
+import 'dart:io' show Platform;
+
+String to_abs_path(path,[base_dir = null]){
+  Path.Context context;
+  if(Platform.isWindows){
+    context = new Path.Context(style:Path.Style.windows);
+  }else{
+    context = new Path.Context(style:Path.Style.posix);
+  }
+  base_dir ??= Path.dirname(Platform.script.toFilePath());
+  path = context.join( base_dir,path);
+  return context.normalize(path);
+}
